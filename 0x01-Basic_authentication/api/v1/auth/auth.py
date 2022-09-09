009f601715936f3c4c5a5b6990fed4bc65267b1a
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-09-09 23:24:08 +0000

    fix

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,7 +9,16 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """returns False - path and excluded_paths"""
-        return (False)
+        if path is None or path not in excluded_paths:
+            return True
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        if path in excluded_paths:
+            return False
+        if '/api/v1/status' in excluded_paths:
+            return False
+        if '/api/v1/status/' in excluded_paths:
+            return False
 
     def authorization_header(self, request=None) -> str:
         """returns None - request"""
