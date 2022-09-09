f454ee912620167a3a7cae4dd6c880db9b18c202
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-09-09 22:48:04 +0000

    Auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Auth class"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """Auth class"""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """returns False - path and excluded_paths"""
+        return (False)
+
+    def authorization_header(self, request=None) -> str:
+        """returns None - request"""
+        return (None)
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """returns None - request will be the Flask request object"""
+        return (None)
