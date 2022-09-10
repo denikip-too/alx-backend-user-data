1d5ac835a9290dcb24ed2ef971cf524d3275c703
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-09-10 16:40:41 +0000

    Request validation

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -15,14 +15,19 @@ class Auth:
             return True
         if path in excluded_paths:
             return False
-        if '/api/v1/status' in excluded_paths:
-            return False
-        if '/api/v1/status/' in excluded_paths:
+        path_a = '/api/v1/status/'
+        for i in excluded_paths:
+            for j in range(0, len(i)):
+                if j.endswith('/') and j.startswith(path_a, 0, 14):
+                    return False
+        if i == '/api/v1/status/' in excluded_paths:
             return False
 
     def authorization_header(self, request=None) -> str:
         """returns None - request"""
-        return (None)
+        if request is None:
+            return None
+        return request.headers.get("Authorization", None)
 
     def current_user(self, request=None) -> TypeVar('User'):
         """returns None - request will be the Flask request object"""
