4b2aaf5479f8a71ee92a48517a0089445d54845e
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-09-10 18:42:12 +0000

    Basic authentication

---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""Auth class"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """Auth class"""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """returns False - path and excluded_paths"""
+        if path is None or path not in excluded_paths:
+            return True
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        if path in excluded_paths:
+            return False
+        path_a = '/api/v1/status/'
+        for i in excluded_paths:
+            for j in range(0, len(i)):
+                if j.endswith('/') and j.startswith(path_a, 0, 14):
+                    return False
+        if i == '/api/v1/status/' in excluded_paths:
+            return False
+
+    def authorization_header(self, request=None) -> str:
+        """returns None - request"""
+        if request is None:
+            return None
+        return request.headers.get("Authorization", None)
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """returns None - request will be the Flask request object"""
+        return None
---
+++ 0x02-Session_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""BasicAuth class"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """BasicAuth class"""
+
+    pass
