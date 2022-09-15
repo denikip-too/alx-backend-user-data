dd9a0f99ed94611b8089d3fd6b664353976d885d
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-09-15 09:45:57 +0000

    push

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""Basic Flask app"""
+from flask import Flask
+from flask import jsonify
+from auth import Auth
+
+
+app = Flask(__name__)
+AUTH = Auth()
+
+
+@app.route('/', method=['GET'])
+def index():
+    """return a JSON payload"""
+    return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', method=['POST'])
+def users(email: str, password: str):
+    """Register user"""
+    try:
+        user = AUTH.register_user(email, password)
+    except NoResultFound:
+        user = AUTH._db.add_user(email, password)
+        return jsonify({"email": f"{email}", "message": "user created"})
+    else:
+        abort(400)
+        return jsonify({"message": "email already registered"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+"""DB module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
+
+from user import Base
+from user import User
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str):
+        """returns a User object.
+        The method should save the user to the database"""
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
+        self._session.commit()
+        return user
+
+    def find_user_by(self, result: str):
+        """returns the first row found in the users table"""
+        try:
+            res = self._session.query(User).one()
+            self._session.commit()
+            return res.result
+        except NoResultFound:
+            raise
+        except InvalidRequestError:
+            raise
+
+    def update_user(self, user_id: int, updt: str) -> None:
+        """takes as argument a required user_id integer
+        and arbitrary keyword arguments and return None"""
+        try:
+            update({find_user_by(user_id): User.updt})
+        except ValueError:
+            raise
