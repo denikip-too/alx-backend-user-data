fb848f7fb0c396d5df640852a79a80eeb365e5d3
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-09-17 13:17:46 +0000

    fix

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -17,11 +17,8 @@ def index() -> str:
 @app.route("/users", methods=['POST'], strict_slashes=False)
 def register_users(email: str, password: str) -> str:
     """Register user"""
-    try:
-        email = request.form['email']
-        password = request.form['password']
-    except KeyError:
-        abort(404)
+    email = request.form['email']
+    password = request.form['password']
 
     try:
         user = AUTH.register_user(email, password)
