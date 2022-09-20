195cdd2782f72e77174936e48a9dea6d70416430
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-09-20 04:00:11 +0000

    fix

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -66,7 +66,7 @@ class Auth:
         if session_id is None:
             return None
         try:
-            user = self._db.find_user_by(email=email)
+            user = self._db.find_user_by(session_id, session_id)
         except NoResultFound:
             return None
         return user
