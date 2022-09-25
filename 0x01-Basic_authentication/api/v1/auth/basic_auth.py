4e93b25f2ffd27f56581f8780a8c0041e87ffea4
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-09-25 22:38:52 +0000

    Basic - Overload current_user

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -50,7 +50,7 @@ class BasicAuth(Auth):
         if re.match('^[:]*$', decoded_base64_authorization_header):
             first = decoded_base64_authorization_header.split(':')[0]
             last = decoded_base64_authorization_header.split(':')[1]
-        return (first, last)
+            return (first, last)
 
     def user_object_from_credentials(
             self, user_email: str, user_pwd: str) -> TypeVar('User'):
